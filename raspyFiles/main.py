1cdb9d73b25e46b7c8c57b54f8e586bc8d507670
julesburette@hotmail.fr <julesburette@hotmail.fr>
2023-05-08 23:55:25 +0200

    test from raspy

--- raspyFiles/main.py
+++ raspyFiles/main.py
@@ -2,6 +2,11 @@ import serial
 import time
 import datetime
 import subprocess
+import board
+import adafruit_dht
+import psutil
+import RPi.GPIO as GPIO
+import requests
 
 class Value:
 
@@ -28,14 +33,14 @@ class Values:
         return f"{res}"
 
 class Arduino:
-    def __init__(self, baud_rate:int = 9600) -> None:
-        self.com_port = self.find_port()
+    def __init__(self,port, baud_rate:int = 9600) -> None:
+        self.com_port = port
         self.baud_rate = baud_rate
         self.ser = serial.Serial(self.com_port, self.baud_rate, timeout = 1)
 
 
     def find_port(self):
-        ports = ['COM{}'.format(i) for i in range(1, 10)]
+        ports = ['/dev/ttyACM{}'.format(i) for i in range(1, 10)]
         for port in ports:
             try:
                 arduino = serial.Serial(port, 9600, timeout=1)
@@ -68,7 +73,7 @@ def checkValues(id):
 
 def sendValuesToArduino(values,memValues):
     "function to send the values the values that has changed to the arduino"
-    for k in range(8):
+    for k in range(len(values)):
         if(checkValues(k)):
             sendValue(values[k],myArduino)
             memValues[k].val = values[k].val
@@ -81,24 +86,46 @@ def makeValues(values):
     values[1].val = time.strftime("%A")
     values[2].val = time.strftime("%B")
     values[3].val = time.strftime('%Y')
-    values[4].val = "temp"
-    values[5].val = "netTemp"
-    values[6].val = "humid"
+    values[4].val = sensor.temperature
+    values[5].val = getWeather()
+    values[6].val = sensor.humidity
     values[7].val = time.strftime('%d')
+    values[8].val = presence
 
 def sendValue(value:Value,arduino:Arduino):
     "function to send a value to the arduino"
     myArduino.ser.write(f"{value.id}={value.val}".encode())
 
+def getTempAndHumid():
+        temp = sensor.temperature
+        humidity = sensor.humidity
+        return temp,humidity
 
 
-myValues = Values([Value("heure",0,""),Value("jour",1,""),Value("mois",2,""),Value("annee",3,""),Value("temp",4,""),Value("netTemp",5,""),Value("humid",6,""),Value("dateJour",7,"")])
-memValues = Values([Value("heure",0,""),Value("jour",1,""),Value("mois",2,""),Value("annee",3,""),Value("temp",4,""),Value("netTemp",5,""),Value("humid",6,""),Value("dateJour",7,"")])
+for proc in psutil.process_iter():
+    if proc.name() == 'libgpiod_pulsein' or proc.name() == 'libgpiod_pulsei':
+        proc.kill()
+
+def getWeather():
+    response = requests.get(url)
+    data = response.json()
+    return data['main']['temp']
+
+myValues = Values([Value("heure",0,""),Value("jour",1,""),Value("mois",2,""),Value("annee",3,""),Value("temp",4,""),Value("netTemp",5,""),Value("humid",6,""),Value("dateJour",7,""),Value("presence",8,"")])
+memValues = Values([Value("heure",0,""),Value("jour",1,""),Value("mois",2,""),Value("annee",3,""),Value("temp",4,""),Value("netTemp",5,""),Value("humid",6,""),Value("dateJour",7,""),Value("presence",8,"")])
 # print(myValues)
-myArduino = Arduino()
+myArduino = Arduino("/dev/ttyACM0")
 # print(myArduino)
 # arduino = serial.Serial(timeout=0)
+sensor = adafruit_dht.DHT11(board.D23)
 memNow = ""
+
+GPIO.setup(4, GPIO.IN)         #Read output from PIR motion sensor
+presence=GPIO.input(4)
+api_key = "5317595bb8e0c71dd94fe21a6512e8f4"
+city_name = "Paris"
+url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}"
+
 time.sleep(3)
 while True:
     time.sleep(1)
@@ -109,6 +136,6 @@ while True:
         sendValuesToArduino(myvalues,mymemvalues)
     # if(len(myArduino.ser.read_all()) > 0):
     #     print("ok")
-
+    print(presence)
     time.sleep(10)
         
